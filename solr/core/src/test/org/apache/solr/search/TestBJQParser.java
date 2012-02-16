5ea6e4a214f4fdba6fc98b640c8b0b0a87a5733f
m-khl <ggedel@gmail.com>
2012-02-17 01:56:38 +0400

    prepare grandchildren data

--- solr/core/src/test/org/apache/solr/search/TestBJQParser.java
+++ solr/core/src/test/org/apache/solr/search/TestBJQParser.java
@@ -2,9 +2,12 @@ package org.apache.solr.search;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Map;
 
 import org.apache.lucene.queryparser.classic.ParseException;
@@ -52,6 +55,7 @@ public class TestBJQParser extends SolrTestCaseJ4 {
                 ;
             }
             Collections.shuffle(block, random);
+            addGrandChildren(block);
             block.add(new String[]{"parent_s",parent});
             blocks.add(block);
         }
@@ -73,57 +77,74 @@ public class TestBJQParser extends SolrTestCaseJ4 {
         assertU(commit());
         assertQ(req("q","*:*"),"//*[@numFound='"+i+"']");
     }
+
+
+    private static void addGrandChildren(List<String[]> block) {
+        List<String> grandChildren = new ArrayList<String>(Arrays.asList("x","y","z"));
+        for(ListIterator<String[]> iter = block.listIterator(); iter.hasNext();){
+            String[] child = iter.next();
+            String child_s = child[1];
+            String parentchild_s = child[3];
+            int grandChildPos=0;
+            while( !grandChildren.isEmpty() && ( 
+                    (grandChildPos = random.nextInt(grandChildren.size()*2)) < grandChildren.size() || 
+                        (!iter.hasNext() && !grandChildren.isEmpty()))){
+                grandChildPos = grandChildPos >=grandChildren.size() ? 0 : grandChildPos; 
+               iter.add(new String[]{
+                    "grand_s", grandChildren.remove(grandChildPos) , 
+                        "grand_child_s",child_s, "grand_parentchild_s",parentchild_s
+               });
+            }
+        }
+        Collections.reverse(block);
+    }
     
     @Test
     public void testFull() throws IOException, Exception{
         String childb = "{!parent filter=\"parent_s:[* TO *]\"}child_s:l";
-        assertQ(req("q",childb), 
-                "//*[@numFound='6']"
-              ,"//doc/arr[@name=\"parent_s\"]/str='a'",
-              "//doc/arr[@name=\"parent_s\"]/str='b'",
-              "//doc/arr[@name=\"parent_s\"]/str='c'",
-              "//doc/arr[@name=\"parent_s\"]/str='d'",
-              "//doc/arr[@name=\"parent_s\"]/str='e'",
-              "//doc/arr[@name=\"parent_s\"]/str='f'"
+        assertQ(req("q",childb), sixParents
               );
     }
+
+    private static final String sixParents [] = new String[]{
+        "//*[@numFound='6']"
+        ,"//doc/arr[@name=\"parent_s\"]/str='a'",
+        "//doc/arr[@name=\"parent_s\"]/str='b'",
+        "//doc/arr[@name=\"parent_s\"]/str='c'",
+        "//doc/arr[@name=\"parent_s\"]/str='d'",
+        "//doc/arr[@name=\"parent_s\"]/str='e'",
+    "//doc/arr[@name=\"parent_s\"]/str='f'"};
     
     @Test
     public void testJustParentsFilter() throws IOException {
         assertQ(req("q", "{!parent filter=\"parent_s:[* TO *]\"}"), 
-                "//*[@numFound='6']"
-              ,"//doc/arr[@name=\"parent_s\"]/str='a'",
-              "//doc/arr[@name=\"parent_s\"]/str='b'",
-              "//doc/arr[@name=\"parent_s\"]/str='c'",
-              "//doc/arr[@name=\"parent_s\"]/str='d'",
-              "//doc/arr[@name=\"parent_s\"]/str='e'",
-              "//doc/arr[@name=\"parent_s\"]/str='f'"
+                sixParents
               );
     }
     
+    private final static String beParents []=  new String[]{
+            "//*[@numFound='2']",
+            "//doc/arr[@name=\"parent_s\"]/str='b'",
+    "//doc/arr[@name=\"parent_s\"]/str='e'"}
+    ;
     @Test
     public void testIntersectBqBjq() {
+        
         assertQ(req("q","+parent_s:(e b) +_query_:\"{!parent filter=$pq v=$chq}\"",
                 "chq","child_s:l",    "pq","parent_s:[* TO *]"), 
-                "//*[@numFound='2']",
-                "//doc/arr[@name=\"parent_s\"]/str='b'",
-                "//doc/arr[@name=\"parent_s\"]/str='e'"
+                beParents 
               );
         assertQ(req("fq","{!parent filter=$pq v=$chq}\"",
                 "q","parent_s:(e b)",
                 "chq","child_s:l",    "pq","parent_s:[* TO *]"), 
-                "//*[@numFound='2']",
-                "//doc/arr[@name=\"parent_s\"]/str='b'",
-                "//doc/arr[@name=\"parent_s\"]/str='e'"
+                beParents
               );
         
         assertQ(req("q","*:*",
                 "fq","{!parent filter=$pq v=$chq}\"",
                 "fq","parent_s:(e b)",
                 "chq","child_s:l",    "pq","parent_s:[* TO *]"), 
-                "//*[@numFound='2']",
-                "//doc/arr[@name=\"parent_s\"]/str='b'",
-                "//doc/arr[@name=\"parent_s\"]/str='e'"
+                beParents
               );
     }
     
@@ -134,9 +155,7 @@ public class TestBJQParser extends SolrTestCaseJ4 {
                 "fq","parent_s:(e b)",
                 "chq","child_s:l",    "pq","parent_s:[* TO *]"//,"debugQuery","on"
                 ),
-                "//*[@numFound='2']",
-                "//doc/arr[@name=\"parent_s\"]/str='b'",
-                "//doc/arr[@name=\"parent_s\"]/str='e'"
+                beParents
               );
 
         boolean qfq = random.nextBoolean();
@@ -144,9 +163,7 @@ public class TestBJQParser extends SolrTestCaseJ4 {
                   (!qfq)? "q":"fq", "{!parent filter=$pq v=$chq}",
                 "chq","parentchild_s:(bm ek cl)",
                 "pq","parent_s:[* TO *]"), 
-                "//*[@numFound='2']",
-                "//doc/arr[@name=\"parent_s\"]/str='b'",
-                "//doc/arr[@name=\"parent_s\"]/str='e'"
+                beParents
         );
         
     }
@@ -157,10 +174,13 @@ public class TestBJQParser extends SolrTestCaseJ4 {
         assertQ(req("q","+parent_s:(a e b) +_query_:\"{!parent filter=$pq v=$chq}\"",
                 "chq","parentchild_s:(bm ek cl)",
                 "pq","parent_s:[* TO *]"), 
-                "//*[@numFound='2']",
-                "//doc/arr[@name=\"parent_s\"]/str='b'",
-                "//doc/arr[@name=\"parent_s\"]/str='e'"
+                beParents
         );
+    }
+    
+    @Test
+    public void testGrandChildren() throws IOException {
+        
     }
     
     @Test
@@ -219,10 +239,12 @@ public class TestBJQParser extends SolrTestCaseJ4 {
         SolrQueryRequest req = req("q", "*:*");
         try{
         
-        assertEquals("empty init args works well", QParser.getParser("{!parent filter=\"parent_s:[* TO *]\"}child_s:l", "", req).getQuery(),
+        assertEquals("empty init args works well",
+                QParser.getParser("{!parent filter=\"parent_s:[* TO *]\"}child_s:l", "", req).getQuery(),
                 QParser.getParser("{!parentemptyinit filter=\"parent_s:[* TO *]\"}child_s:l", "", req).getQuery());
         
-        assertEquals("empty init args works well for parent filter mode", QParser.getParser("{!parent filter=\"parent_s:[* TO *]\"}", "", req).getQuery(),
+        assertEquals("empty init args works well for parent filter mode too",
+                QParser.getParser("{!parent filter=\"parent_s:[* TO *]\"}", "", req).getQuery(),
                 QParser.getParser("{!parentemptyinit filter=\"parent_s:[* TO *]\"}", "", req).getQuery());
         }finally{
             req.close();
